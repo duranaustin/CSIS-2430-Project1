6892c58eb0dbe500562e52112a2aab6aceb03658
Austin Duran <austin.duran39@gmail.com>
2020-02-24 12:53:18 -0700

    comment fuzzy sets out
    comment fuzzy sets out

--- src/Projects/ProjectOneQuestion_3.java
+++ src/Projects/ProjectOneQuestion_3.java
@@ -1,50 +1,93 @@
 package Projects;
 import Resources.Calculate;
-import java.util.HashSet;
+import java.util.HashMap;
 
 /**
  * @Authors: Jorge Jimenez, Pauline Makoma, Christian Makoma, Austin Duran
- * @Project 1 Question 3
+ * @Project 1 Question 1
  * @Course CSIS-2430
  */
 public class ProjectOneQuestion_3 extends Calculate {
     //Sets -- mutable
-    HashSet<Integer> fuzzySetA = new HashSet(); //for Q3 'fuzzy set A'
-    HashSet<Integer> fuzzySetB = new HashSet(); //for Q3 'fuzzy set B'
+    HashMap fuzzySet = new HashMap(); //for Q1 'set of n elements'
+    HashMap fuzzySetA = new HashMap<>(); //for Q1 'subset A'
+    HashMap fuzzySetB = new HashMap<>(); //for Q1 'subset B'
 
     //Arrays -- immutable once set
-    Object[] fuzzySetaArray; //for Q3 'fuzzy set A'
-    Object[] fuzzySetbArray; //for Q3 'fuzzy set B'
+    Object[] fuzzySetArray; //for Q1 'set of n elements'
+    Object[] fuzzySetaArray; //for Q1 'subset A'
+    Object[] fuzzySetbArray; //for Q1 'subset B'
 
-    //Question/Computer Project 3 - Fuzzy Sets
-    HashSet<Integer> Q3answerComplementA; //for Q3 - '`A'
-    HashSet<Integer> Q3answerUnionAB; //for Q3 - 'A U B'
-    HashSet<Integer> Q3answerIntersectionAB; //for Q3 - 'A ^ B'
+    //Question/Computer Project 1 - Subsets
+    HashMap Q3answerComplementA; //for Q1 - '`A'
+    HashMap Q3answerUnionAB; //for Q1 - 'A U B'
+    HashMap Q3answerIntersectionAB; //for Q1 - 'A ^ B'
+
 
 
     public ProjectOneQuestion_3(){
-        setFuzzySets();
+        setFuzzySetA(); //hardcoded integer set from 0-9
+        setFuzzySetB(); //hardcoded integer set from 0-9
         initAnswerSets(); //initialize answer sets
-        findComplementOfA(fuzzySetA, fuzzySetB, fuzzySetaArray, fuzzySetbArray); //find answer to question 1.
-        findUnionAB(fuzzySetA, fuzzySetB, fuzzySetaArray, fuzzySetbArray); //find answer to question 2.
-        findIntersectionAB(fuzzySetA, fuzzySetB, fuzzySetaArray, fuzzySetbArray); //find answer to question 3.
-    }
-
-    private void setFuzzySets(){
-        //..whatever a fuzzy set is...
+//        Q3answerComplementA = findComplementOfAFuzzySet(fuzzySetA, fuzzySetB, fuzzySetaArray, fuzzySetbArray); //find answer to  Q3 - '`A'TODO
+//        Q3answerUnionAB = findUnionABFuzzySet(fuzzySetA, fuzzySetB, fuzzySetaArray, fuzzySetbArray); //find answer to Q3 - 'A U B'TODO
+//        Q3answerIntersectionAB = findIntersectionABFuzzySet(fuzzySetA, fuzzySetB, fuzzySetaArray, fuzzySetbArray); //find answer to Q3 - 'A ^ B'TODO
     }
 
     /**
      * Initialization of answer sets
      */
     private void initAnswerSets() {
-        //Question/Computer Project 3 - Fuzzy Sets
-        Q3answerComplementA = new HashSet();
-        Q3answerUnionAB = new HashSet();
-        Q3answerIntersectionAB = new HashSet();
+        //Question/Computer Project 1 - Subsets
+        Q3answerComplementA = new HashMap();
+        Q3answerUnionAB = new HashMap();
+        Q3answerIntersectionAB = new HashMap();
     }
 
-    public String getAnswer() {
-        return null;
+    /**
+     * setSet sets the Set from 0-9
+     * setArray is also initialized
+     */
+    public void setFuzzySetA() {
+        for (int i = 0; i < 10; i++){
+            fuzzySetA.put(random.nextDouble(),i);
+        }
+    }
+
+    /**
+     * setSet sets the Set from 0-9
+     * setArray is also initialized
+     */
+    public void setFuzzySetB() {
+        for (int i = 0; i < 10; i++){
+            fuzzySet.put(random.nextDouble(),i);
+        }
+    }
+
+    /**
+     * getAnswer
+     * @return
+     */
+    public void getAnswer() {
+        System.out.println("Question 3 - 'Complement of A' used Set: ");
+        System.out.println(fuzzySetA.toString());
+        System.out.println("Question 3 - 'Complement of A' answer is: ");
+        System.out.println("A = " + Q3answerComplementA.toString());
+        System.out.println();
+        System.out.println();
+        System.out.println("Question 3 - 'A UNION B' used Sets: ");
+        System.out.println("A = " +fuzzySetA.toString());
+        System.out.println("B = " +fuzzySetB.toString());
+        System.out.println("Question 3 - 'A UNION B' answer is: ");
+        System.out.println(Q3answerUnionAB.toString());
+        System.out.println();
+        System.out.println();
+        System.out.println("Question 3 - 'A INTERSECTION B' used Sets: ");
+        System.out.println("A = " +fuzzySetA.toString());
+        System.out.println("B = " +fuzzySetB.toString());
+        System.out.println("Question 3 - 'A INTERSECTION B' answer is: ");
+        System.out.println(Q3answerIntersectionAB.toString());
+        System.out.println();
+        System.out.println();
     }
 }
--- src/Resources/Calculate.java
+++ src/Resources/Calculate.java
@@ -1,6 +1,7 @@
 package Resources;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 /**
@@ -12,12 +13,15 @@ public class Calculate extends Tools{
 
     //Question/Computer Project 1 - Subsets
     HashSet<Integer> answerComplementA = new HashSet(); //for - '`A'
+    HashMap answerComplementAFuzzy = new HashMap(); //for - '`A'
     HashSet<Integer> answerUnionAB = new HashSet(); //for - 'A U B'
     ArrayList answerUnionABArray = new ArrayList(); //for - 'A U B'
+    HashMap answerUnionABFuzzy = new HashMap(); //for - 'A U B'
     HashSet<Integer> answerAPlusB = new HashSet(); //for - 'A + B'
     ArrayList answerAPlusBArray = new ArrayList(); //for - 'A + B'
     HashSet<Integer> answerIntersectionAB = new HashSet(); //for - 'A ^ B'
     ArrayList answerIntersectionABArray = new ArrayList(); //for - 'A ^ B'
+    HashMap answerIntersectionABFuzzy = new HashMap(); //for - 'A ^ B'
     HashSet<Integer> answerDifferenceAB = new HashSet(); //for - 'A - B'
     ArrayList answerDifferenceABArray = new ArrayList(); //for - 'A - B'
     HashSet<Integer> answerDifferenceABForSymmetric = new HashSet();; //for - 'A - B' for symmetric calculation
@@ -37,6 +41,20 @@ public class Calculate extends Tools{
 
     String answer = "";
     SetTools setTools = new SetTools();
+
+//    /**TODO
+//     * findComplementOfAFuzzy finds the complement for fuzzy sets
+//     * this is exclusive to the integer universe
+//     * @return
+//     */
+//    public HashMap findComplementOfAFuzzySet(HashMap setA, HashMap setB, Object[] arrayA, Object[] arrayB) {
+//        for(int i = 0; i < 15; i++){
+//            if(setA.get(i)){
+//                this.answerComplementAFuzzy.add(i);
+//            }
+//        }
+//        return this.answerComplementAFuzzy;
+//    }
 
     /**
      * findComplementOfA finds the complement
@@ -67,6 +85,22 @@ public class Calculate extends Tools{
         }
         return this.answerUnionAB;
     }
+
+//    /**TODO
+//     * findUnionAB finds the union for fuzzy sets
+//     * @return
+//     */
+//    public HashMap findUnionABFuzzySet(HashMap setA, HashMap setB, Object[] arrayA, Object[] arrayB) {
+//        for(int i = 0; i < arrayA.length; i++){
+//            this.answerUnionABFuzzy.add((Integer) arrayA[i]); //add all of what's in setA
+//        }
+//        for(int i = 0; i < arrayB.length; i++){
+//            if(!setA.contains(arrayB[i])){
+//                this.answerUnionABFuzzy.add((Integer) arrayB[i]); //then add all of what's in setB that's not in setA
+//            }
+//        }
+//        return this.answerUnionABFuzzy;
+//    }
 
     /**
      * findUnionABArray finds the union for Arrays
@@ -122,6 +156,29 @@ public class Calculate extends Tools{
         }
         return this.answerIntersectionAB;
     }
+
+//    /**TODO
+//     * findIntersectionAB finds the intersection
+//     * @return
+//     */
+//    public HashMap findIntersectionABFuzzySet(HashMap setA, HashMap setB, Object[] arrayA, Object[] arrayB) {
+//        largestArrayLength = findLargestSetLength(arrayA, arrayB); //put initialization of other setTools here until we find a better way
+//        if(setAIsLargest){
+//            for(int i = 0; i < largestArrayLength; i++){
+//                if(setB.contains(arrayA[i])){
+//                    this.answerIntersectionABFuzzy.add((Integer) arrayA[i]); //then add all of what's in setB that's not in setA
+//                }
+//            }
+//        }
+//        if(setBIsLargest) {
+//            for (int i = 0; i < largestArrayLength; i++) {
+//                if (setA.contains(arrayB[i])) {
+//                    this.answerIntersectionABFuzzy.add((Integer) arrayB[i]); //then add all of what's in setB that's not in setA
+//                }
+//            }
+//        }
+//        return this.answerIntersectionABFuzzy;
+//    }
 
     /**
      * findIntersectionAB finds the intersection
